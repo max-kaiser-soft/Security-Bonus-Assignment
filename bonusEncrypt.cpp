92de162081468105ad6c9dcb0849e783d960efe8
MAX KAISER <39889089+max-kaiser-soft@users.noreply.github.com>
2025-08-25 16:26:33 -0400

    Add files via upload

---
+++ bonusEncrypt.cpp
@@ -0,0 +1,147 @@
+//MAX KAISER
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <bitset>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+vector<int> keyMaker() { // method to make the key
+    srand(time(0));
+
+    vector<int> output(8); // allocate memory for vector of integers
+
+    for (int c = 0; c < 8; c++) {
+        int num = rand() % 2; // generate number 0 or 1
+        output[c] = num;} // add num to output
+
+    return output;
+}
+
+
+int keyDecimalMaker(vector<int> key){ //converts key binary to decimal
+        int output = 0;
+    
+    for (int c = 7; c >= 0; c--){
+        int keyVal = key[c] - 0; 
+
+        //cout<<keyVal<<"*"<<pow(2, (7 - c))<<" ";
+        //cout<<keyVal * pow(2, (7 - c))<< "   ";
+
+        output += keyVal * pow(2, (7 - c));}
+
+    return output;
+}
+
+
+char* messageMaker(char* input, int keyDecimal, int length) { //method to make the message
+
+    char* output = new char[length]; //allocate memory for c-string
+    output[length] = '\0'; //null terminator
+
+    for (int c = 0; c < length; c++){ //loop to go through full message
+        
+        char inputChar = input[c]; //grab character
+        int inputCharASCII = int(inputChar); //convert character to ASCII
+        
+        int outputCharACSII = inputCharASCII ^ keyDecimal; //XOR with key
+        
+        char outputChar = static_cast<char>(outputCharACSII); //convert result to character
+
+        output[c] = outputChar; //add to output
+
+    }
+        //char toAdd[2] = "0";
+        //strncat(output, toAdd, 1); //add num to output    
+    
+    return output;
+}
+
+
+int printer(char* content, const char* file){ //prints to file
+
+    ofstream outfile(file); //create and open text file
+
+    if (!outfile.is_open()) {// check if file is open
+        cerr << "Failed to open file for writing.\n";
+        return 1;
+    }
+
+    outfile << content; // write to  file
+
+    outfile.close(); //close file
+
+    return 0;
+}
+
+char* keyToCStr(vector<int> key){ //key as cstring
+
+    char* output = new char[8]; //allocate memory for c-string
+    output[8] = '\0'; //null terminator
+
+    for (int c = 0; c < 8; c++){ //loop to go through full message
+
+        char outputChar = '0' + key[c];
+        output[c] = outputChar; //add to output
+    }
+        
+    return output;
+}
+
+
+char* readFile(const char* file){ //read file to get letter
+    
+    ifstream inputFile(file);
+
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file!" << endl;}
+
+    stringstream buffer; //read file into stringstream buffer
+
+    buffer << inputFile.rdbuf();
+
+    string fileContent = buffer.str(); //convert buffer into a single string
+
+    //cout << fileContent << endl; //print file content
+
+    inputFile.close();
+
+    
+    char* output = new char[fileContent.length() + 1]; //allocate memory for c-string
+    output[fileContent.length()] = '\0'; //null terminator
+
+    for (int c = 0; c < fileContent.length(); c++){
+        output[c] = fileContent[c];}
+
+    return output;
+}
+
+
+int main(){ //main
+
+    char* input = readFile("letter.txt"); //read file to get letter
+
+    //char input[] = "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Natures God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation."; //temp input
+    vector<int> key = keyMaker(); //function to make key
+    char* cStrKey = keyToCStr(key); //key as cstring
+
+    int keyDecimal = keyDecimalMaker(key); //key as decimal number
+
+
+    char* message = messageMaker(input, keyDecimal, strlen(input)); //make message
+
+    printer(message, "secret.txt"); //printers
+    printer(cStrKey, "key.txt");
+
+    cout<<"Message written to secret.txt"<<endl<<"Key written to key.txt"<<endl; //print confirmation 
+
+    return 0;
+}
